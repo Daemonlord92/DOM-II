34c615ee25c30962fc2bc950341e0648ef9d5be6
Matthew Martin <zhintateo19924559@gmail.com>
2020-03-13 22:14:58 -0500

    completed the project

--- js/index.js
+++ js/index.js
@@ -4,8 +4,9 @@ let imgHide = document.querySelector('.intro img')
 const navStop = document.querySelectorAll('.nav-link')
 const imgFlip = document.querySelector('.img-content img')
 const body = document.querySelector('body')
-const desitnationColor = document.querySelectorAll('.destination')
+const desitnation = document.querySelectorAll('.destination')
 const headerColorChange = document.querySelectorAll('h2')
+const contentDesitnation = document.querySelector('.content-destination')
 
 //Events
 imgHide.addEventListener('mouseenter', function(){
@@ -26,10 +27,18 @@ imgFlip.addEventListener('dblclick', () => {
 	imgFlip.style.transform = "scaleX(1)";
 })
 
+imgFlip.addEventListener('mouseover', () => {
+	imgFlip.style.transform = 'scale(1.2)';
+	imgFlip.style.transition = 'all 0.5s';
+})
+
+imgFlip.addEventListener('mouseleave', () => {
+	imgFlip.style.transform = 'scale(1)';
+})
+
 navStop.forEach(title => {
 	title.addEventListener('click', (event) => {
 		event.preventDefault()
-		console.log('Debug')
 	})
 })
 
@@ -42,7 +51,11 @@ body.addEventListener('dblclick', () => {
 	body.style.backgroundColor = 'white';
 })
 
-desitnationColor.forEach(title => {
+body.onresize = (event) => {
+	body.style.backgroundColor = 'blue';
+}
+
+desitnation.forEach(title => {
 	title.addEventListener('click', (event) => {
 		event.target.style.backgroundColor = 'purple';
 		event.target.style.color = 'red';
@@ -53,5 +66,28 @@ headerColorChange.forEach(title => {
 	title.addEventListener('click', (event) => {
 		event.target.style.color = '#967272'
 		event.target.transition = 'all 0.3s'
+	})
+})
+
+headerColorChange.forEach(title => {
+	title.addEventListener('mouseover', (event) => {
+		event.target.style.opacity = '0.5';
+		event.target.style.transition = 'all o.3s';
+	})
+})
+
+contentDesitnation.addEventListener('drag', (event) =>{
+	event.target.style.backgroundColor = 'pink';
+})
+
+contentDesitnation.addEventListener('copy', (event) => {
+	event.target.style.color = 'blue';
+	event.target.style.fontWeight = 'bold';
+})
+
+headerColorChange.forEach(title => {
+	title.addEventListener('cut', (event) => {
+		event.target.style.backgroundColor = 'black';
+		event.target.style.color = 'white';
 	})
 })
